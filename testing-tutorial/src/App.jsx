f42ffada22975dfd2e704a8a8d79f6df3ce43a52
GabrielBRQ <gabrielmedvalle@gmail.com>
2026-02-10 16:35:55 -0300

    finish testing

---
+++ testing-tutorial/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import User from './User';
+
+function App() {
+  const [user, setUser] = React.useState(null);
+  const [error, setError] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+      .then((response) => response.json())
+      .then((user) => setUser(user))
+      .catch((error) => setError(error.message));
+  }, []);
+
+  if (error) {
+    return <span>{error}</span>;
+  }
+
+  return <div>{user ? <User user={user} /> : <span>Loading...</span>}</div>;
+}
+
+export default App;
